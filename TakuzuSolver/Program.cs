0c1605ec06f77e96b430a825a45a03ead03ff0ba
protoman9000 <oyekan900@yahoo.com>
2015-10-27 22:21:20 -0700

    Creating a grid for the puzzle, trying to decide on which size to strat with.

--- TakuzuSolver/Program.cs
+++ TakuzuSolver/Program.cs
@@ -10,6 +10,18 @@ namespace TakuzuSolver
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting Takuza Solver");
+            Console.ReadKey();
+            
+            Console.WriteLine("Enter the height:");
+            string tmp = Console.ReadLine();
+            int tmpNum = Convert.ToInt16(tmp);
+
+            Console.WriteLine("Enter the Width:");
+            string tmp2 = Console.ReadLine();
+            int tmp2Num = Convert.ToInt16(tmp2);
+            
+            string[,] newGrid = new string[tmp2Num, tmpNum];
         }
     }
 }
